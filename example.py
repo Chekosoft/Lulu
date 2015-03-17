18adba34c6f0996b018e2a77e7dbf317bddaa3ea
Joaquín Muñoz M <Chekosoft@users.noreply.github.com>
2015-03-17 01:48:09 -0300

    Example is now updated with route parameter handling

--- example.py
+++ example.py
@@ -4,9 +4,14 @@ from Lulu import App as Lulu
 
 
 with Lulu(u'/', u'index'):
-  def get(request):
-    return u'Yup, that tasted purple.'
+    def get(request):
+        Lulu.logger.info('Route Name is %s', request.route_params['route_name'])
+        return u'Yup, that tasted purple.'
 
+with Lulu(u'/{name:word}', u'name'):
+    def get(request):
+        Lulu.logger.info('Route Name is %s', request.route_params['route_name'])
+        return u'Hi, %s' % request.route_params['name']
 
 if __name__ == '__main__':
     Lulu.start()
