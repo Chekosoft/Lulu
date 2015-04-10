789bd6bcbe075add7269053fb797ac605b77ed4e
Joaquín Muñoz M <Chekosoft@users.noreply.github.com>
2015-04-10 02:27:47 -0300

    Updated example file

--- example.py
+++ example.py
@@ -6,9 +6,14 @@ from Lulu import App as Lulu
 with Lulu(u'/', u'index'):
     def get(request):
         Lulu.logger.info('Route Name is %s', request.route_params['route_name'])
+        if not 'times' in request.session:
+            request.session['times'] = 1
+        else:
+            request.session['times'] += 1
+        Lulu.logger.info('Accesed %d times' % request.session['times'])
         return u'Yup, that tasted purple.'
 
-with Lulu(u'/{name:word}', u'name'):
+with Lulu(u'/{name:word}', 'name'):
     def get(request):
         Lulu.logger.info('Route Name is %s', request.route_params['route_name'])
         return u'Hi, %s' % request.route_params['name']
