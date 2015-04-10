3b3150156a779c7c90f64f9d5477dec2ae7ba01c
Joaquín Muñoz M <Chekosoft@users.noreply.github.com>
2015-04-10 02:31:56 -0300

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,20 @@
+"""
+#Lulu
+
+A web microframework inspired by Cuba, from Ruby.
+"""
+from setuptools import setup
+
+setup(
+    name=u'Lulu',
+    version=u'0.0.1',
+    license=u'MIT',
+    author=u'Joaquín Muñoz',
+    install_requires=[
+        u'webob>=1.4',
+        u'wheezy.routing>=0.1.157',
+        u'beaker>=1.6.5.post1'
+    ],
+    packages=[u'Lulu'],
+    platforms=u'any'
+)
