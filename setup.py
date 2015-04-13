c6d76a2697c5e6fb106dbe3d0d48448447c848bf
Joaquín Muñoz M <Chekosoft@users.noreply.github.com>
2015-04-13 16:36:16 -0300

    removed frozendict from requirements

--- setup.py
+++ setup.py
@@ -15,8 +15,7 @@ setup(
     install_requires=[
         u'webob>=1.4',
         u'wheezy.routing>=0.1.157',
-        u'beaker>=1.6.5.post1',
-        u'frozendict>=0.4'
+        u'beaker>=1.6.5.post1'
     ],
     packages=[u'Lulu'],
     platforms=u'any'
