1cba7402b1c2b8f67b6f34e1b1255def27b1ef35
Joaquín Muñoz M <Chekosoft@users.noreply.github.com>
2015-04-13 16:38:12 -0300

    Updated example.py to reflect new changes

--- example.py
+++ example.py
@@ -2,21 +2,25 @@
 
 from Lulu import App as Lulu
 
+Lulu.config({
+    'show_x_powered_by': False #Removes "X-Powered-By: Lulu" header from response headers
+})
+
 
 with Lulu(u'/', u'index'):
     def get(request):
-        Lulu.logger.info('Route Name is %s', request.route_params['route_name'])
+        Lulu.logger.info('Route Name is %s', (request.route_params['route_name']))
         if not 'times' in request.session:
             request.session['times'] = 1
         else:
             request.session['times'] += 1
-        Lulu.logger.info('Accesed %d times' % request.session['times'])
+        Lulu.logger.info('Accesed %d times' % (request.session['times']))
         return u'Yup, that tasted purple.'
 
 with Lulu(u'/{name:word}', 'name'):
     def get(request):
-        Lulu.logger.info('Route Name is %s', request.route_params['route_name'])
-        return u'Hi, %s' % request.route_params['name']
+        Lulu.logger.info('Route Name is %s', (request.route_params['route_name']))
+        return u'Hi, %s' % (request.route_params['name'])
 
 if __name__ == '__main__':
     Lulu.start()
